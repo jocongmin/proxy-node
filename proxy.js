ea8b989f0462662463e7b5765017e83e4115b00d
zhoucm <zhoucm@ename.com>
2017-11-08 14:34:53 +0800

    proxy vue dist

--- proxy.js
+++ proxy.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var request = require('request');
 var app = express();
-app.use('/', function(req, res) {
+
+app.use('/', express.static(__dirname + '/dist'))
+
+app.use('/v1', function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    var url = 'http://wx.ename.com' + req.url;
+    var url = 'http://newescrowadmin.ename.cn/v1' + req.url;
+    console.log(url,"url")
     req.pipe(request(url)).pipe(res);
 });
 app.listen(process.env.PORT || 3000); 
